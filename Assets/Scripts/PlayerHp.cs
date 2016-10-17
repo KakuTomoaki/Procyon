c998180ded10689c978d958b3b2b5a1f1d182d9a
KakuTomoaki <wit-life@hotmail.co.jp>
2016-10-18 00:53:02 +0900

    v1.0.1
    3ヶ月ぶりの更新ｗ
    ■更新内容
    　・Unityちゃん削除 → 開発レシピのazalea使用
    　・WASD移動実装
    　・スペースでジャンプ(今後ブーストに変更予定ｗ)
    　・体力実装(HPが動的に減少_5割以下黄色_3割以下赤)
    　・見やすくするためMapの簡易実装(適当)

---
+++ Assets/Scripts/PlayerHp.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHp : MonoBehaviour {
+
+    public static int armorPoint;
+    int armorPointMax = 5000;
+
+    int damage = 100;
+
+    public Text armorText;
+
+    int displayArmorPoint;
+    
+	// Use this for initialization
+	void Start () {
+        armorPoint = armorPointMax;
+        displayArmorPoint = armorPoint;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //現在の体力と表示用体力が異なっていれば、現在の体力になるまで加減算する
+        if(displayArmorPoint != armorPoint) {
+            displayArmorPoint = (int)Mathf.Lerp(displayArmorPoint, armorPoint, 0.1f);
+        }
+
+        //体力をUI Textに表示する
+        armorText.text = string.Format("{0:0000}", displayArmorPoint, armorPointMax);
+
+        //残り体力の割合により文字の色を変える
+        float percentageArmorPoint = (float)displayArmorPoint / armorPointMax;
+
+        if(percentageArmorPoint > 0.5f) {
+            armorText.color = Color.white;
+        } else if(percentageArmorPoint > 0.3f) {
+            armorText.color = Color.yellow;
+        } else {
+            armorText.color = Color.red;
+        }	
+	}
+
+    private void OnCollisionEnter(Collision collider) {
+
+        //敵の攻撃が接触時ダメージ
+        if(collider.gameObject.tag == "testEnemy") {
+            armorPoint -= damage;
+            armorPoint = Mathf.Clamp(armorPoint, 0, armorPointMax);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 15.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //プレイヤーを移動させる
+        CharacterController controller = GetComponent<CharacterController>();
+
+        if(controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if(Input.GetButton("Jump")) {
+                moveDirection.y = jumpSpeed;
+                PlayerHp.armorPoint -= 300;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+	
+	}
+}
