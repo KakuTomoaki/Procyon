e1ec25f8a059ca7df1eaac9b35b1d3086ef0863d
KakuTomoaki <wit-life@hotmail.co.jp>
2016-12-16 00:36:21 +0900

    V1.0.7
    ■更新内容
    　カメラ調整(マウスのX軸回転を0 → キャラの回転のみでX軸を回転させました)

--- Assets/Scripts/CameraRotate2.cs
+++ Assets/Scripts/CameraRotate2.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraRotate2 : MonoBehaviour {
     
     public float RotationSensitivity = 100f;// 感度
+    public GameObject target_P;
                                             // Use this for initialization
     void Start () {
 
@@ -13,13 +14,14 @@ public class CameraRotate2 : MonoBehaviour {
     void Update() {
 
         GameObject CameraParent = Camera.main.transform.parent.gameObject;
+        target_P = GameObject.Find("azalea");
 
         var rotX = Input.GetAxis("Mouse X") * Time.deltaTime * RotationSensitivity;
         var rotY = Input.GetAxis("Mouse Y") * Time.deltaTime * RotationSensitivity;
-
-        //var lookAt = Target.position + Vector3.up * HeightM;
         
-        CameraParent.transform.Rotate(Vector3.up, rotX, 0);
+        
+        //PlayerRotateでX軸を回転してるので↓はコメントアウト
+        //CameraParent.transform.Rotate(Vector3.up, rotX, 0);
         CameraParent.transform.Rotate(rotY, 0, 0);
 
         
--- Assets/Scripts/PlayerRotate.cs
+++ Assets/Scripts/PlayerRotate.cs
@@ -5,6 +5,7 @@ public class PlayerRotate : MonoBehaviour {
 
     public GameObject target_C;
     public GameObject target_P;
+    public float RotationSensitivity = 200f;// 感度
 
     // Use this for initialization
     void Start () {
@@ -15,8 +16,10 @@ public class PlayerRotate : MonoBehaviour {
     void Update() {
         target_C = GameObject.Find("CameraParent");
         target_P = GameObject.Find("azalea");
-        //この辺のコードで調整出来るはず…
-        transform.rotation = Quaternion.Euler(0, target_C.transform.localEulerAngles.y, 0);
+        var rotX = Input.GetAxis("Mouse X") * Time.deltaTime * RotationSensitivity;
+        //キャラの回転
+        transform.Rotate(Vector3.up, rotX * 1.5f, 0);
+        //transform.rotation = Quaternion.Euler(0, target_C.transform.localEulerAngles.y, 0);
         //transform.rotation = Quaternion.Euler(0, target_C.transform.localEulerAngles.y - target_P.transform.localEulerAngles.y, 0);
         //this.transform.Rotate(0, target.transform.localEulerAngles.y, 0);
 
